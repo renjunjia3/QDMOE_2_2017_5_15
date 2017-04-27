ca8fb8a8496317509f5ee8f33f671f32246c1867
renjunjia3 <376124750@qq.com>
2017-04-27 13:22:09 +0800

    升级包

--- app/src/main/java/com/ofgvyiss/ofgvyi/config/PayConfig.java
+++ app/src/main/java/com/ofgvyiss/ofgvyi/config/PayConfig.java
@@ -8,5 +8,5 @@ import com.ofgvyiss.ofgvyi.app.App;
 
 public class PayConfig {
     //当前应用版本号
-    public static final String VERSION_NAME = "1.1.2-2017/4/27-Relase-" + App.CHANNEL_ID;
+    public static final String VERSION_NAME = "1.1.3-2017/4/27-Relase-" + App.CHANNEL_ID;
 }
