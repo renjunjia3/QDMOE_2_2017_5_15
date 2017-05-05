d4c3c3d8b8a3eed98609c7370651d2e2ab57fae4
renjunjia3 <376124750@qq.com>
2017-05-05 11:04:56 +0800

    修改试看页的问题

--- app/src/main/java/com/ebcnke/knulg/config/PayConfig.java
+++ app/src/main/java/com/ebcnke/knulg/config/PayConfig.java
@@ -8,5 +8,5 @@ import com.ebcnke.knulg.app.App;
 
 public class PayConfig {
     //当前应用版本号
-    public static final String VERSION_NAME = "1.2.7-2017/5/4-Relase-" + App.CHANNEL_ID;
+    public static final String VERSION_NAME = "1.2.9-2017/5/5-Relase-" + App.CHANNEL_ID;
 }
